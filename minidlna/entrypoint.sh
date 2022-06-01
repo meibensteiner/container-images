efb5ac6b84d7a5f6f7df7cc6eeda2f257ea42de6
Michael Eibensteiner <68381547+meibensteiner@users.noreply.github.com>
2022-06-01 13:37:57 +0200

    Test

--- minidlna/entrypoint.sh
+++ minidlna/entrypoint.sh
@@ -4,4 +4,7 @@ if [ -e /run/minidlna/minidlna.pid ]; then
     rm /run/minidlna/minidlna.pid
 fi
 
-exec /usr/sbin/minidlnad -S
+printenv | grep minidlna_ | sed -e "s/^minidlna_//" > /tmp/overrides.ini
+awk -F= 'NR==FNR{a[$1]=$0;next}($1 in a){$0=a[$1]}1' /tmp/overrides.ini /etc/minidlna-template.conf > /etc/minidlna.conf
+
+exec /usr/sbin/minidlnad  -S
